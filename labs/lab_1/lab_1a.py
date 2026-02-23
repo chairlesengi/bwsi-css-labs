1c8c33a095934383dfa6647ab43fc85afd04c56c
chairlesengi <70824018+chairlesengi@users.noreply.github.com>
2026-02-23 14:37:08 +0800

    add introduction to lab1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Charles Jaffe" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(f"Hello all! My name is Charles and I am a junior at Poly Prep Country Day School in Brooklyn, New York. I enjoy electronics, mechanical engineering, and music. I am also a member of my school's FTC robotics team, where I do a lot of the CAD and design work for the robot.")
 
 if __name__ == "__main__":
     main()
